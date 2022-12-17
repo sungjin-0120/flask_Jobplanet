240a88d5da750b19665718ed74ffbf342c570232
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2022-12-18 01:02:11 +0900

    Delete job.py

--- job.py
+++
@@ -1,8 +0,0 @@
-def save_to_file(file_name, pls):
-  with open(f"{file_name}.csv","w", encoding="utf8" ) as FIle:
-    FIle.write("Company, Location, Occupation, Size, Exp, Link\n")
-
-    for pl in pls:
-      FIle.write(
-        f"{pl['company']},{pl['region']},{pl['occupation']},{pl['size']},{pl['exp']},{pl['link']}\n"
-      )
